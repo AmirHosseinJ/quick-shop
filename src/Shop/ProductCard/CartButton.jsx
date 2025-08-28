b2ccdfca2d6d7f5493b6498fffa8daba3b8281fc
Rhythm <amir.amir566.aj@gmail.com>
2025-08-28 21:13:17 +0330

    cart btn UI changed

--- src/Shop/ProductCard/CartButton.jsx
+++ src/Shop/ProductCard/CartButton.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef} from "react";
 import {useCart} from "../CartContext/CartContext";
+import './CartButton.css'
+import cartIcon from "../../assets/check-it-out.svg";
 
 export default function CartButton() {
     const {count} = useCart();
@@ -7,19 +9,47 @@ export default function CartButton() {
 
     // If Offcanvas isn’t mounted, button still shows badge
     return (
-        <div className="sticky-bottom pb-3">
-            <button
+        <div className="fixed-bottom">
+            {/*<button*/}
+            {/*    ref={btnRef}*/}
+            {/*    type="button"*/}
+            {/*    className="btn btn-primary cart-fab sticky-bottom "*/}
+            {/*    data-bs-toggle="offcanvas"*/}
+            {/*    data-bs-target="#cartOffcanvas"*/}
+            {/*    aria-controls="cartOffcanvas"*/}
+            {/*>*/}
+            {/*    🛒*/}
+            {/*    {count > 0 && <span className="badge bg-light text-dark ms-2">{count}</span>}*/}
+            {/*</button>*/}
+            <a
+                className="custom-btn square"
+                id="custom-homebut"
                 ref={btnRef}
-                type="button"
-                className="btn btn-primary cart-fab sticky-bottom "
                 data-bs-toggle="offcanvas"
                 data-bs-target="#cartOffcanvas"
-                aria-controls="cartOffcanvas"
             >
-                🛒
-                {count > 0 && <span className="badge bg-light text-dark ms-2">{count}</span>}
-            </button>
+
+                <span className="custom-icon-container ">
+                   <img
+                       src={cartIcon}
+                       alt="Cart Icon"
+                       className="custom-icon-cart-check "
+                   />
+                    {count > 0 &&
+                        // <span className="badge bg-light text-dark ms-2">
+                        //     {count}
+                        // </span>
+                        <span
+                            className="position-absolute top-0 start-0 translate-middle badge rounded-pill  btn-qty-badge">
+                        {count}
+                    </span>
+                    }
+
+                </span>
+            </a>
+
         </div>
+
 
     );
 }
